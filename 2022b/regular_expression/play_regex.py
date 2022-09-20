021e4ca13ff7944c6bf7e74c4638ee28546e02b0
Tou7 <houj0411@gmail.com>
2022-09-20 11:16:32 +0800

    [2022b] add japanese example for regex

--- 2022b/regular_expression/play_regex.py
+++ 2022b/regular_expression/play_regex.py
@@ -12,10 +12,11 @@ def match_english():
     print(patt_2.findall(x))
     print(patt_3.findall(x))
 
-zh_1 = re.compile(r"[\u4E00-\u9FFF]") # CJK Unified Ideographs: 4E00-9FFF
-zh_2 = re.compile(r"[ㄅ-龜]")
+# Unicode range of Mandarin: 4E00-9FFF
+zh_1 = re.compile(r"[\u4E00-\u9FFF]") 
+zh_2 = re.compile(r"[ㄅ-龜]") # Taiwan people style
 zh_3 = re.compile(r"[\u4E00-\u9FFF]|[A-z]+") # Use or to include English words.
-zh_4 = re.compile(r"[\u4E00-\u9FFF]+|[A-z]+") # Use or to include English words.
+zh_4 = re.compile(r"[ㄅ-龜]+|[A-z]+") # Use or to include English words.
 
 def match_mandarin():
     x = "你也是Gundam嗎"
@@ -25,7 +26,18 @@ def match_mandarin():
     print(zh_3.findall(x))
     print(zh_4.findall(x))
 
+# Unicode range of Japanese: 4e00-9fbf, 3040-309f and 30a0-30ff
+jp_1 = re.compile(r"[\u4e00-\u9fbf]|[\u3040-\u309f]|[\u30a0-\u30ff]") 
+jp_2 = re.compile(r"[\u4e00-\u9fbf]+|[\u3040-\u309f]+|[\u30a0-\u30ff]+")
+def match_japanese():
+    x = "お前もガンダムですか"
+    print(patt_1.findall(x))
+    print(zh_1.findall(x))
+    print(jp_1.findall(x))
+    print(jp_2.findall(x))
+
 if __name__ == "__main__":
     # match_english()
-    match_mandarin()
+    # match_mandarin()
+    match_japanese()
 
