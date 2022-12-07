8db28c8a45006648006c38cca9151a53e09c0f71
Tou7 <houj0411@gmail.com>
2022-12-07 13:57:50 +0800

    2022b: update edit distance

--- 2022b/edit_distance/example.py
+++ 2022b/edit_distance/example.py
@@ -1,12 +1,11 @@
-import edit_distance
+"""
+Install:
+    pip install edit-distance
 
-def get_edit_distance(string_1, string_2):
-    ref = list(string_1)
-    hyp = list(string_2)
-    sm = edit_distance.SequenceMatcher(a=ref, b=hyp)
-    codes = sm.get_opcodes()
-    print(codes)
-    return len(codes)
+Reference:
+    https://pypi.org/project/edit-distance/
+"""
+import edit_distance
 
 def get_edit_distance_w(sent1, sent2):
     ref = sent1.split(" ")
@@ -16,7 +15,6 @@ def get_edit_distance_w(sent1, sent2):
 
     print(codes)
     return len(codes)
-
 
 print(get_edit_distance('leda', 'deal'))
 print(get_edit_distance('leda', 'deala'))
